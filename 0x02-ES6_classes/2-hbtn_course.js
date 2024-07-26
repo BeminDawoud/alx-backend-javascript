06c13fd90dcbf2533f54e67f9e1ea466b47dfe21
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-07-27 02:09:14 +0300

    Create task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,46 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  /* setters  */
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  set students(value) {
+    if (
+      Array.isArray(value)
+      && value.every((element) => typeof element === 'string')
+    ) {
+      this._students = value;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
+  }
+
+  /* getters  */
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._length;
+  }
+}
